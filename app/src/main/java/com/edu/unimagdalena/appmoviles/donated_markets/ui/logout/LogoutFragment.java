23c64b153e338ef9e1f94ac1c41ad4f82fcc43ef
davp00 <daniielvp82@gmail.com>
2020-10-26 19:39:39 -0500

    sharedpreferences agregado

--- app/src/main/java/com/edu/unimagdalena/appmoviles/donated_markets/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/edu/unimagdalena/appmoviles/donated_markets/ui/logout/LogoutFragment.java
@@ -1,6 +1,8 @@
 package com.edu.unimagdalena.appmoviles.donated_markets.ui.logout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.edu.unimagdalena.appmoviles.donated_markets.LoginActivity;
 import com.edu.unimagdalena.appmoviles.donated_markets.R;
@@ -28,7 +31,17 @@ public class LogoutFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Intent i = new Intent(getContext(), LoginActivity.class);
-        requireActivity().startActivity(i);
-        requireActivity().finish();
+
+        FragmentActivity fragmentActivity = requireActivity();
+
+        SharedPreferences sharedPreferences = fragmentActivity.getSharedPreferences(
+            getString(R.string.user_shared_preferences),
+            Context.MODE_PRIVATE
+        );
+
+        sharedPreferences.edit().clear().apply();
+
+        fragmentActivity.startActivity(i);
+        fragmentActivity.finish();
     }
 }
