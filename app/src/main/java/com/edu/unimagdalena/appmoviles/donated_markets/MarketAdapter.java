c7ca128aa7d6d87179ea77ef605b4b4b50b4bc8f
rompni <andresnavarrodev@gmail.com>
2020-10-26 23:10:20 -0500

    filtrar por barrio

--- app/src/main/java/com/edu/unimagdalena/appmoviles/donated_markets/MarketAdapter.java
+++ app/src/main/java/com/edu/unimagdalena/appmoviles/donated_markets/MarketAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,17 +15,20 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.TimeZone;
+import java.util.Locale;
 
 public class MarketAdapter extends ArrayAdapter<Market> {
 
+    private TextView neighborhood, homeaddress, person, code, date;
+    private ArrayList<Market> markets, copyMarkets;
+
     public MarketAdapter(@NonNull Context context, @NonNull ArrayList<Market> objects) {
-        super(context,0, objects);
+        super(context, 0, objects);
+        this.markets = objects;
+        this.copyMarkets = new ArrayList<>();
+        this.copyMarkets.addAll(markets);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -34,32 +38,49 @@ public class MarketAdapter extends ArrayAdapter<Market> {
 
         Market m = getItem(position);
 
-        if(convertView == null)
+        if (convertView == null)
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.market_view, parent, false);
 
-        TextView neighborhood = (TextView) convertView.findViewById(R.id.txtNeighborhood);
-        TextView homeaddress = (TextView) convertView.findViewById(R.id.txtHomeAddress);
-        TextView person = (TextView) convertView.findViewById(R.id.txtPerson);
-        TextView code = (TextView) convertView.findViewById(R.id.txtCode);
-        TextView date = (TextView) convertView.findViewById(R.id.txtDatetime);
+        neighborhood = convertView.findViewById(R.id.txtNeighborhood);
+        homeaddress = convertView.findViewById(R.id.txtHomeAddress);
+        person = convertView.findViewById(R.id.txtPerson);
+        code = convertView.findViewById(R.id.txtCode);
+        date = convertView.findViewById(R.id.txtDatetime);
 
-        if(m != null) {
+        if (m != null) {
             try {
                 neighborhood.setText(m.getMarketNeighborhood());
                 homeaddress.setText(m.getMarketHomeAddress());
                 person.setText(m.getMarketPersonReceives());
                 code.setText(String.valueOf(m.getMarketID()));
 
-               long timestamp = Long.parseLong(m.getMarketDatetime());
+                long timestamp = Long.parseLong(m.getMarketDatetime());
                 Date expiry = new Date(timestamp * 1000L);
                 @SuppressLint("SimpleDateFormat") SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ");
 
                 date.setText(jdf.format(expiry));
-            }catch (Exception ex){
-                System.out.println( " ERRORES " + ex.getMessage());
+            } catch (Exception ex) {
+                System.out.println(" ERRORES " + ex.getMessage());
             }
 
         }
         return convertView;
+    }
+
+    public void filter(String text) {
+        text = text.toLowerCase();
+        markets.clear();
+
+        if (text.length() == 0) {
+            markets.addAll(copyMarkets);
+        } else {
+
+            for (Market m : copyMarkets) {
+                if (m.getMarketNeighborhood().toLowerCase().contains(text)) {
+                    markets.add(m);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/edu/unimagdalena/appmoviles/donated_markets/ui/donation_list/DonationListFragment.java
+++ app/src/main/java/com/edu/unimagdalena/appmoviles/donated_markets/ui/donation_list/DonationListFragment.java
@@ -2,9 +2,12 @@ package com.edu.unimagdalena.appmoviles.donated_markets.ui.donation_list;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,12 +21,14 @@ import com.edu.unimagdalena.appmoviles.donated_markets.MarketController;
 import com.edu.unimagdalena.appmoviles.donated_markets.R;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class DonationListFragment extends Fragment {
 
     ListView mklist;
     MarketController mc;
     ArrayList<Market> arrayOfMarkets;
+    EditText search;
     Market m;
     Context c;
 
@@ -38,16 +43,40 @@ public class DonationListFragment extends Fragment {
         //final TextView textView = view.findViewById(R.id.text_gallery);
         //textView.setText("Lista de Donaciones");
         c = view.getContext();
-
         mc = new MarketController(c);
         m = new Market(100, "Santa Cruz", "Calle 47B",
                 "1081126732", "Julio Mario");
+
+        mc.addMarket(new Market(200, "Santa Cruz X", "Calle 47B",
+                "1081126732", "Julio Mario"));
+
+        mc.addMarket(new Market(300, "Santa Ana", "Calle 47B",
+                "1081126732", "Julio Mario"));
         if (mc.getMarket(m.getMarketID()) == null)
             mc.addMarket(m);
 
         arrayOfMarkets= new ArrayList<>(mc.getAllMarkets());
-        MarketAdapter adapter = new MarketAdapter(c, arrayOfMarkets);
+        final MarketAdapter adapter = new MarketAdapter(c, arrayOfMarkets);
         mklist = (ListView) view.findViewById(R.id.marketList);
         mklist.setAdapter(adapter);
+
+        search = view.findViewById(R.id.neighborhood_find);
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String text = search.getText().toString();
+                adapter.filter(text);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
+
 }
